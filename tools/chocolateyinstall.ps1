127ffe2d96438e9de97d730f776fab6c5ed3ee78
Joan Viladrosa <joan@moderne.io>
2023-12-19 10:15:47 +0100

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $version    = "v$env:ChocolateyPackageVersion"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$toolsDir\\mod.exe"
-  url           = "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-windows/v$version/moderne-cli-windows-v$version"
+  url           = "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-windows/$version/moderne-cli-windows-$version"
 
   checksum      = '85A5BC579BC53A356ABB667F4B9FD1831BA7859EF79E3B1454C37F7E933636CA'
   checksumType  = 'sha256'
