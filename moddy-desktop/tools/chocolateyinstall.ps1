e0f5ce854926fa536f9a20620b675c49312f04e1
Nate <nate@moderne.io>
2025-05-29 10:04:14 -0700

    chore: add moddy desktop chocolatey package

---
+++ moddy-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$version = "v$env:ChocolateyPackageVersion".Split("-")[0].TrimStart("v")
+$filename = "moddy-$version-setup.exe"
+$url = "https://moddy-desktop.s3.us-east-1.amazonaws.com/releases/$version/$filename"
+$installer = Join-Path $toolsDir $filename
+
+$packageArgs = @{
+  packageName    = 'moddy-desktop'
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Moddy Desktop*'
+}
+
+Install-ChocolateyPackage @packageArgs
