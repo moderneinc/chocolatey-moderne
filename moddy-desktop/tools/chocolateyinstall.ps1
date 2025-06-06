d88e01f69a0f09ad53f588a9686b959027828a9d
Nate Danner <nate@moderne.io>
2025-06-06 07:27:20 -0700

    fix: update moddy desktop chocolatey path

--- moddy-desktop/tools/chocolateyinstall.ps1
+++ moddy-desktop/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $version = "v$env:ChocolateyPackageVersion".Split("-")[0].TrimStart("v")
 $filename = "moddy-$version-setup.exe"
-$url = "https://moddy-desktop.s3.us-east-1.amazonaws.com/releases/$version/$filename"
+$url = "https://moddy-desktop.s3.us-east-1.amazonaws.com/releases/$filename"
 $installer = Join-Path $toolsDir $filename
 
 $packageArgs = @{
